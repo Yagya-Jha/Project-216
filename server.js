02b568eb430c5bbbd1486afaa0ad1bab1523a55f
Yagya Jha <74773203+Yagya-Jha@users.noreply.github.com>
2023-06-23 16:21:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const {v4: uuidv4} = require("uuid")
-
 const io = require("socket.io")(server, {cors: {origin: '*'}});
 
 app.get("/", (req, res) => {
